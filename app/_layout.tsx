50f774971baa7f83348a7fca945f451563166fdc
Julio Macedo <juliocesar1076@gmail.com>
2023-06-25 18:38:30 -0300

    feat: create layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,21 @@
-import { Stack } from "expo-router";
-
+import { View, StyleSheet } from "react-native";
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 export default function Layout() {
-  return <Stack screenOptions={{}} />;
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Slot />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 24,
+    backgroundColor: "#0000ff",
+  },
+});

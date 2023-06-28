b1c7fc1e5b8286b4fee8ae07d79dfb07451c1f41
Julio Macedo <juliocesar1076@gmail.com>
2023-06-28 15:23:33 -0300

    feat: home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,10 @@
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <Text>home</Text>
+    <View className="flex-1 justify-center items-center">
+      <Link href="/login">go to login</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 24,
-  },
-});

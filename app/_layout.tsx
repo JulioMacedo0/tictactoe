5b4e93c51a0a5eba6b434d9ec6c760cbb66a5467
Julio Macedo <juliocesar1076@gmail.com>
2023-06-28 14:49:24 -0300

    feat: remove theme provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,5 @@
 import { Stack } from "expo-router";
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./themes/default";
 
 export default function Layout() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Stack initialRouteName="/login" screenOptions={{}} />
-    </ThemeProvider>
-  );
+  return <Stack initialRouteName="/login" screenOptions={{}} />;
 }

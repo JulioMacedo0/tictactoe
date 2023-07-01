fc39696d2b51c5055c3ce43322dc560b68196e38
Julio Macedo <juliocesar1076@gmail.com>
2023-07-01 11:18:56 -0300

    feat: init layoyt login page

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,9 +1,27 @@
-import { View, Text } from "react-native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, Image, TouchableOpacity, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Login() {
   return (
-    <View className="flex-1 items-center justify-center ">
-      <Text>Login</Text>
-    </View>
+    <LinearGradient
+      colors={["#fb832c", "#fb4764"]}
+      className="flex-1 items-center justify-center "
+    >
+      <StatusBar style="light" />
+      <Stack.Screen
+        options={{
+          header: () => null,
+        }}
+      />
+      <Text className="text-white text-4xl mb-8">Tic Tac </Text>
+
+      <TouchableOpacity className="bg-white  w-[60%] px-4 py-2 rounded-full ">
+        <Text className="text-black text-center text-lg font-bold ">
+          sig in
+        </Text>
+      </TouchableOpacity>
+    </LinearGradient>
   );
 }

5d0153ea929101371b563953cec768823e823b9b
Julio Macedo <juliocesar1076@gmail.com>
2023-07-01 12:03:16 -0300

    feat: redirect page

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,7 @@
-import { Link } from "expo-router";
+import { Link, Redirect, Stack } from "expo-router";
 import { StyleSheet, View } from "react-native";
 
 export default function Page() {
-  return (
-    <View className="flex-1 justify-center items-center">
-      <Link href="/login">go to login</Link>
-    </View>
-  );
+  const useAuth = false;
+  return useAuth ? <Redirect href="/home" /> : <Redirect href="/login" />;
 }

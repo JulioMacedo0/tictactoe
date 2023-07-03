7e364bdc817380c2630f4cb2161d4bd4f931041e
Julio Macedo <juliocesar1076@gmail.com>
2023-07-03 16:18:32 -0300

    feat: create tab home

---
+++ app/(tab)/home.tsx
@@ -0,0 +1,10 @@
+import { Link, Stack, Tabs } from "expo-router";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function Page() {
+  return (
+    <View className="">
+      <Text>HOME</Text>
+    </View>
+  );
+}

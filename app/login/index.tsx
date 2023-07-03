5474db38c16266c4166d9e0a3c39f987ae40eca0
Julio Macedo <juliocesar1076@gmail.com>
2023-07-03 16:18:57 -0300

    feat: navigate to home

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,23 +1,24 @@
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, Image, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useRouter } from "expo-router";
 
 export default function Login() {
+  const route = useRouter();
   return (
     <LinearGradient
       colors={["#fb832c", "#fb4764"]}
       className="flex-1 items-center justify-center "
     >
       <StatusBar style="light" />
-      <Stack.Screen
-        options={{
-          header: () => null,
-        }}
-      />
+
       <Text className="text-white text-4xl mb-8">Tic Tac </Text>
 
-      <TouchableOpacity className="bg-white  w-[60%] px-4 py-2 rounded-full ">
+      <TouchableOpacity
+        className="bg-white  w-[60%] px-4 py-2 rounded-full"
+        onPress={() => route.replace("home")}
+      >
         <Text className="text-black text-center text-lg font-bold ">
           sig in
         </Text>

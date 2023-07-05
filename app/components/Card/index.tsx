d48a92530c29a9347b0ea70cd127f535e5622ed8
Julio Macedo <juliocesar1076@gmail.com>
2023-07-05 11:45:39 -0300

    feat: put gradient in card

--- app/components/Card/index.tsx
+++ app/components/Card/index.tsx
@@ -1,12 +1,15 @@
 import { View, Text } from "react-native";
-
+import { LinearGradient } from "expo-linear-gradient";
 interface CardProps {}
 
 const Card = () => {
   return (
-    <View className="bg-gold w-[80%] h-[50px] rounded-xl justify-center">
+    <LinearGradient
+      colors={["#fdcb5f", "#fda538"]}
+      className="bg-gold w-[80%] h-[50px] rounded-xl justify-center"
+    >
       <Text className="text-base text-black text-center font-bold">75</Text>
-    </View>
+    </LinearGradient>
   );
 };
 

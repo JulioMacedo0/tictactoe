d59b6ca73234abb1bd23f30a86e3830bf074c3eb
Julio Macedo <juliocesar1076@gmail.com>
2023-07-09 20:54:17 -0300

    feat: remove

--- app/index.tsx
+++ app/index.tsx
@@ -2,5 +2,5 @@ import { Redirect } from "expo-router";
 
 export default function Page() {
   const useAuth = false;
-  return useAuth ? <Redirect href="/home" /> : <Redirect href="/login" />;
+  return useAuth ? <Redirect href="home" /> : <Redirect href="login" />;
 }

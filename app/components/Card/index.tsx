71c145fab28c707761a988dad3d4004fe6273b40
Julio Macedo <juliocesar1076@gmail.com>
2023-07-09 20:54:37 -0300

    feat: update card component

--- app/components/Card/index.tsx
+++ app/components/Card/index.tsx
@@ -1,23 +1,30 @@
-import { View, Text } from "react-native";
+import { Pressable, Text, TouchableOpacity } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 interface CardProps {
   className?: string;
   variant: "gold" | "rose";
+  text: string;
+  onPress?: () => void;
 }
 
-const Card = ({ className, variant }: CardProps) => {
+const Card = ({ className, variant, text, onPress }: CardProps) => {
   const goldGradient = ["#fdcb5f", "#fda538"];
   const roseGradient = ["#f85577", "#f22551"];
 
   const arrayColors = variant == "gold" ? goldGradient : roseGradient;
   const textColor = variant == "gold" ? "text-black" : "text-white";
   return (
-    <LinearGradient
-      colors={arrayColors}
-      className={`bg-gold w-[80%] h-[50px] rounded-xl justify-center ${className}`}
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={onPress}
+      className={`w-[80%] h-[50px] rounded-xl overflow-hidden ${className}`}
     >
-      <Text className={`text-base text-center font-bold ${textColor}`}>75</Text>
-    </LinearGradient>
+      <LinearGradient colors={arrayColors} className="flex-1 justify-center">
+        <Text className={`text-base text-center font-bold ${textColor}`}>
+          {text}
+        </Text>
+      </LinearGradient>
+    </TouchableOpacity>
   );
 };
 

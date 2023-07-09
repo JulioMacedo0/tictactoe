8aa54425881b0e94e0174639c989b264d7caf1ca
Julio Macedo <juliocesar1076@gmail.com>
2023-07-09 14:00:01 -0300

    feat: add props

--- app/components/Card/index.tsx
+++ app/components/Card/index.tsx
@@ -1,12 +1,14 @@
 import { View, Text } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-interface CardProps {}
+interface CardProps {
+  className?: string;
+}
 
-const Card = () => {
+const Card = ({ className }: CardProps) => {
   return (
     <LinearGradient
       colors={["#fdcb5f", "#fda538"]}
-      className="bg-gold w-[80%] h-[50px] rounded-xl justify-center"
+      className={`bg-gold w-[80%] h-[50px] rounded-xl justify-center ${className}`}
     >
       <Text className="text-base text-black text-center font-bold">75</Text>
     </LinearGradient>

d1ac2b57b0f81e40d578faab42d13c9ce6e92afe
Julio Macedo <juliocesar1076@gmail.com>
2023-07-09 20:53:55 -0300

    feat: create game page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="playground/index"
+        options={{
+          headerShown: true,
+          title: "Online Game",
+          headerStyle: {
+            backgroundColor: "#f4511e",
+          },
+        }}
+      />
+    </Stack>
+  );
 }

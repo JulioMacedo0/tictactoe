b4f37e7c7ad0a0ee1efcbd09e43ca4d676a23d7b
Julio Macedo <juliocesar1076@gmail.com>
2023-07-10 17:14:28 -0300

    feat: change folder

---
+++ src/app/playground.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function PlayGround() {
+  return (
+    <View className="flex-1 bg-background">
+      <Text>Play</Text>
+    </View>
+  );
+}
+
+export default PlayGround;

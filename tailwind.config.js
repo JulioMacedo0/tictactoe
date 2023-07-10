b18b286eb3347d9b762c884ebd362b748fecb25e
Julio Macedo <juliocesar1076@gmail.com>
2023-07-10 10:32:07 -0300

    feat: import corlos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import colors from "../../themes/colors";
+
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        primary: "#fed168",
-        secundary: "#fa2c5d",
-        gold: "#fcb345",
-        background: "#fee4ea",
-      },
+      colors,
     },
   },
   plugins: [],

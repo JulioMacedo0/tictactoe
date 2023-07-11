ea862ba28ac4f04d8231f5a708921f424b9dc778
Julio Macedo <juliocesar1076@gmail.com>
2023-07-11 12:04:50 -0300

    feat: add border

--- src/components/Tic-Tac-Toe/index.tsx
+++ src/components/Tic-Tac-Toe/index.tsx
@@ -2,22 +2,24 @@ import { View, TouchableOpacity, StyleSheet } from "react-native";
 
 export const TicTacToe = () => {
   return (
-    <View className="h-[40%] w-[90%] mx-auto">
-      <View className="flex-1 flex-row">
-        <TouchableOpacity className="flex-1 bg-secundary border-2 border-white border-dashed"></TouchableOpacity>
-        <TouchableOpacity className="flex-1 bg-secundary"></TouchableOpacity>
-        <TouchableOpacity className="flex-1 bg-secundary"></TouchableOpacity>
-      </View>
+    <View className="h-[40%] w-[90%] mx-auto mt-8 ">
+      <View className="flex-1 bg-white rounded-2xl p-1 border-2 border-secundary border-dashed">
+        <View className="flex-1 flex-row ">
+          <TouchableOpacity className="flex-1 bg-secundary rounded-tl-2xl"></TouchableOpacity>
+          <TouchableOpacity className="flex-1 bg-secundary"></TouchableOpacity>
+          <TouchableOpacity className="flex-1 bg-secundary rounded-tr-3xl"></TouchableOpacity>
+        </View>
 
-      <View className="flex-1 flex-row">
-        <TouchableOpacity className="flex-1 bg-secundary"></TouchableOpacity>
-        <TouchableOpacity className="flex-1 bg-secundary border-2 border-white border-dashed"></TouchableOpacity>
-        <TouchableOpacity className="flex-1 bg-secundary"></TouchableOpacity>
-      </View>
-      <View className="flex-1 flex-row">
-        <TouchableOpacity className="flex-1 bg-secundary"></TouchableOpacity>
-        <TouchableOpacity className="flex-1 bg-secundary"></TouchableOpacity>
-        <TouchableOpacity className="flex-1 bg-secundary"></TouchableOpacity>
+        <View className="flex-1 flex-row">
+          <TouchableOpacity className="flex-1 bg-secundary "></TouchableOpacity>
+          <TouchableOpacity className="flex-1 bg-secundary "></TouchableOpacity>
+          <TouchableOpacity className="flex-1 bg-secundary"></TouchableOpacity>
+        </View>
+        <View className="flex-1 flex-row">
+          <TouchableOpacity className="flex-1 bg-secundary rounded-bl-2xl"></TouchableOpacity>
+          <TouchableOpacity className="flex-1 bg-secundary"></TouchableOpacity>
+          <TouchableOpacity className="flex-1 bg-secundary rounded-br-2xl"></TouchableOpacity>
+        </View>
       </View>
     </View>
   );

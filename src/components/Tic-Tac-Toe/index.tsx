4d24fcde3cb9c11e347652cd6ace78bee5a01358
Julio Macedo <juliocesar1076@gmail.com>
2023-07-12 16:23:52 -0300

    feat: add style

--- src/components/Tic-Tac-Toe/index.tsx
+++ src/components/Tic-Tac-Toe/index.tsx
@@ -1,32 +1,52 @@
-import { View, TouchableOpacity, StyleSheet } from "react-native";
+import { useCorlos } from '@/hooks/useColors';
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  TouchableHighlight,
+  Button,
+  Pressable,
+} from 'react-native';
 
 export const TicTacToe = () => {
   return (
-    <View className="h-[40%] w-[90%] mx-auto mt-8 ">
-      <View className="flex-1 bg-white rounded-2xl p-1 border-2 border-secundary border-dashed">
-        <View className="flex-1 flex-row ">
-          <TouchableOpacity className="flex-1 bg-secundary rounded-tl-2xl"></TouchableOpacity>
-          <TouchableOpacity className="flex-1 bg-secundary"></TouchableOpacity>
-          <TouchableOpacity className="flex-1 bg-secundary rounded-tr-3xl"></TouchableOpacity>
-        </View>
+    <View className="h-[40%] w-[90%] mx-auto mt-8">
+      <View
+        className="flex-1 bg-white p-1 overflow-hidden"
+        style={styles.border}
+      >
+        <View className="flex-1 p-4 bg-secundary rounded-2xl overflow-hidden divide-y-2 divide-dashed divide-white">
+          <View className="flex-1 flex-row divide-x-2 divide-dashed divide-white">
+            <TouchableOpacity
+              className="flex-1 bg-secundary "
+              onPress={() => console.log('tapped 1')}
+            />
+            <TouchableOpacity className="flex-1 bg-secundary " />
+            <TouchableOpacity className="flex-1 bg-secundary " />
+          </View>
 
-        <View className="flex-1 flex-row">
-          <TouchableOpacity className="flex-1 bg-secundary "></TouchableOpacity>
-          <TouchableOpacity className="flex-1 bg-secundary "></TouchableOpacity>
-          <TouchableOpacity className="flex-1 bg-secundary"></TouchableOpacity>
-        </View>
-        <View className="flex-1 flex-row">
-          <TouchableOpacity className="flex-1 bg-secundary rounded-bl-2xl"></TouchableOpacity>
-          <TouchableOpacity className="flex-1 bg-secundary"></TouchableOpacity>
-          <TouchableOpacity className="flex-1 bg-secundary rounded-br-2xl"></TouchableOpacity>
+          <View className="flex-1  flex-row divide-x-2 divide-dashed divide-white">
+            <TouchableOpacity className="flex-1 bg-secundary " />
+            <TouchableOpacity className="flex-1 bg-secundary " />
+            <TouchableOpacity className="flex-1 bg-secundary " />
+          </View>
+
+          <View className="flex-1 flex-row divide-x-2 divide-dashed divide-white">
+            <TouchableOpacity className="flex-1 bg-secundary " />
+            <TouchableOpacity className="flex-1 bg-secundary " />
+            <TouchableOpacity className="flex-1 bg-secundary " />
+          </View>
         </View>
       </View>
     </View>
   );
 };
-
+const { secundary } = useCorlos();
 const styles = StyleSheet.create({
-  gap: {
-    gap: 6,
+  border: {
+    borderRadius: 15,
+    borderStyle: 'dashed',
+    borderWidth: 2,
+    borderColor: secundary,
   },
 });

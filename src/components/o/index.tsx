e0dd848b83cdb4504028338c6f33eb867dbf5f8a
Julio Macedo <juliocesar1076@gmail.com>
2023-07-13 16:26:30 -0300

    feat: creat O component

---
+++ src/components/o/index.tsx
@@ -0,0 +1,9 @@
+import { useCorlos } from "@/hooks/use-colors";
+import { Octicons } from "@expo/vector-icons";
+
+interface OProps {
+  size: number;
+}
+export const O = ({ size }: OProps) => {
+  return <Octicons name="circle" size={size} color="#fff" />;
+};

5fce323baa3d7f8acc0362bcbecc3a935a0e46d3
Julio Macedo <juliocesar1076@gmail.com>
2023-07-13 16:25:24 -0300

    feat: add profiles

--- src/app/playground.tsx
+++ src/app/playground.tsx
@@ -1,10 +1,31 @@
-import { TicTacToe } from "@/components/Tic-Tac-Toe";
-import { View, Text } from "react-native";
+import { Profile } from "@/components/profile";
+import { RoundCounter } from "@/components/round-counter";
+import { TicTacToe } from "@/components/tic-tac-toe";
+
+
+import { View } from "react-native";
 
 function PlayGround() {
   return (
     <View className="flex-1 bg-background">
+      <View className="flex-row px-6 justify-between  mt-4">
+        <Profile
+          imageUrl="https://github.com/JulioMacedo0.png"
+          userName="Julio Macedo"
+          cardClasName="w-[120px] h-[135px]"
+          playWith="X"
+        />
+        <RoundCounter roundCount={1}/>
+        <Profile
+          imageUrl="https://github.com/wendelfreitas.png"
+          userName="Wendel Freitas"
+          cardClasName="w-[120px] h-[135px]"
+          playWith="O"
+        />
+      </View>
+
       <TicTacToe />
+      
     </View>
   );
 }

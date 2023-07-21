0b4d3bc8f40ff46468dbbf79e743e0afeb9e2231
Julio Macedo <juliocesar1076@gmail.com>
2023-07-21 16:52:52 -0300

    feat: get session

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -2,9 +2,23 @@ import { View } from "react-native";
 import { Profile } from "@components/profile";
 import { Card } from "@/components/card";
 import { useRouter } from "expo-router";
+import { supabase } from "@/supabase/init";
+import { useEffect, useState } from "react";
+import { Session } from "@supabase/supabase-js";
 
 export default function HomePage() {
   const navigate = useRouter();
+  const [user, setUser] = useState<Session>(null);
+
+  const getAuthSession = async () => {
+    const { data } = await supabase.auth.getSession();
+    setUser(data.session);
+  };
+  useEffect(() => {
+    getAuthSession();
+  }, []);
+
+  console.log(user);
   return (
     <View className="flex-1 items-center bg-background ">
       <Profile

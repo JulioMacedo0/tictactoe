3b5c26e8b35a5792c2a3516b4575c9e48efd765e
Julio Macedo <juliocesar1076@gmail.com>
2023-07-21 11:20:38 -0300

    feat: create register page

---
+++ src/app/register.tsx
@@ -0,0 +1,93 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useRouter } from "expo-router";
+import { useColors } from "@hooks/use-colors";
+import { useState } from "react";
+import { supabase } from "@/supabase/init";
+import { Card } from "@/components/card";
+import { Input } from "@/components/Input";
+import { Feather, MaterialIcons, Octicons } from "@expo/vector-icons";
+
+export default function Register() {
+  const route = useRouter();
+
+  const { primary, secundary } = useColors();
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function login() {
+    setLoading(true);
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+    console.log(data);
+    console.log(error);
+    if (!error && !data.user) {
+      setLoading(false);
+      alert("Check your email for the login link!");
+    }
+    if (error) {
+      setLoading(false);
+      alert(error.message);
+    }
+  }
+  return (
+    <LinearGradient
+      colors={[primary, secundary]}
+      className="flex-1 items-center justify-center"
+      style={{
+        gap: 12,
+      }}
+    >
+      <StatusBar style="light" />
+      <Input
+        icon={<Feather name="user" size={24} color="white" />}
+        variant="rose"
+        placeholder="Enter your full name"
+        value={email}
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardType="email-address"
+        onChangeText={(text) => setEmail(text)}
+      />
+
+      <Input
+        icon={<MaterialIcons name="email" size={24} color="white" />}
+        variant="rose"
+        placeholder="Enter your email"
+        value={email}
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardType="email-address"
+        onChangeText={(text) => setEmail(text)}
+      />
+
+      <Input
+        icon={<Octicons name="key" size={24} color="white" />}
+        variant="rose"
+        placeholder="Enter your password"
+        value={password}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry={true}
+        onChangeText={(text) => setPassword(text)}
+      />
+
+      <Card
+        text={loading ? "Loading..." : "Create account"}
+        variant="rose"
+        onPress={login}
+      />
+      <View className="flex-row ">
+        <Text className="text-white text-base "> have account?</Text>
+        <TouchableOpacity onPress={() => route.replace("/login")}>
+          <Text className="text-primary text-base "> Click here</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
+  );
+}

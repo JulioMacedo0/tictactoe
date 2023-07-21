901f3b3d74fa9bb10a41f66c7a1374e82e3e747b
Julio Macedo <juliocesar1076@gmail.com>
2023-07-21 11:20:59 -0300

    feat:  add icon prop

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,14 @@
 import { useColors } from "@/hooks/use-colors";
 import { LinearGradient } from "expo-linear-gradient";
-import React, { useState } from "react";
-import { TextInput, TextInputProps } from "react-native";
+import React, { ReactNode, useState } from "react";
+import { TextInput, TextInputProps, View } from "react-native";
 
 interface InputProps extends TextInputProps {
   variant: "gold" | "rose";
+  icon: ReactNode;
 }
 
-export const Input: React.FC<InputProps> = ({ variant, ...props }) => {
+export const Input: React.FC<InputProps> = ({ variant, icon, ...props }) => {
   const { primary, secundary, lightPrimary, lightSecundary } = useColors();
 
   const goldGradient = [lightPrimary, primary];
@@ -19,10 +20,14 @@ export const Input: React.FC<InputProps> = ({ variant, ...props }) => {
   return (
     <LinearGradient
       colors={arrayColors}
-      className="w-[80%] h-[50px] rounded-xl"
+      style={{
+        gap: 8,
+      }}
+      className="w-[80%] h-[50px] rounded-3xl flex-row items-center px-4"
     >
+      <View>{icon}</View>
       <TextInput
-        className={`flex-1 px-4 ${textColor}`}
+        className={`flex-1 ${textColor}`}
         placeholderTextColor={placeHolderTextColor}
         {...props}
       />

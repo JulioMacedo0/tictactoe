73253427dafa5b1b05256bf9fceaca1db0926447
Julio Macedo <juliocesar1076@gmail.com>
2023-07-21 10:02:24 -0300

    feat: add disable prop

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -6,9 +6,16 @@ interface CardProps {
   variant: "gold" | "rose";
   text: string;
   onPress?: () => void;
+  disable?: boolean;
 }
 
-export const Card = ({ style, variant, text, onPress }: CardProps) => {
+export const Card = ({
+  style,
+  variant,
+  text,
+  onPress,
+  disable = false,
+}: CardProps) => {
   const { primary, secundary, lightPrimary, lightSecundary } = useColors();
   const goldGradient = [lightPrimary, primary];
   const roseGradient = [lightSecundary, secundary];
@@ -20,6 +27,7 @@ export const Card = ({ style, variant, text, onPress }: CardProps) => {
       activeOpacity={0.8}
       onPress={onPress}
       className={`w-[80%] h-[50px] rounded-xl overflow-hidden ${style}`}
+      disabled={disable}
     >
       <LinearGradient colors={arrayColors} className="flex-1 justify-center">
         <Text className={`text-base text-center font-bold ${textColor}`}>

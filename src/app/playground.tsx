f42b96124d88d2fae3f008b98a955abbea582cfe
Julio Macedo <juliocesar1076@gmail.com>
2023-08-11 16:55:32 -0300

    feat: get user data

--- src/app/playground.tsx
+++ src/app/playground.tsx
@@ -1,21 +1,22 @@
 import { Profile } from "@/components/profile";
 import { RoundCounter } from "@/components/round-counter";
 import { TicTacToe } from "@/components/tic-tac-toe";
-
+import { useAuth } from "@/context/authContext";
 
 import { View } from "react-native";
 
 function PlayGround() {
+  const { user } = useAuth();
   return (
     <View className="flex-1 bg-background">
       <View className="flex-row px-6 justify-between  mt-4">
         <Profile
           imageUrl="https://github.com/JulioMacedo0.png"
-          userName="Julio Macedo"
+          userName={`${user.user_metadata.username}`}
           cardClasName="w-[120px] h-[135px]"
           playWith="X"
         />
-        <RoundCounter roundCount={1}/>
+        <RoundCounter roundCount={1} />
         <Profile
           imageUrl="https://github.com/wendelfreitas.png"
           userName="Wendel Freitas"
@@ -25,7 +26,6 @@ function PlayGround() {
       </View>
 
       <TicTacToe />
-      
     </View>
   );
 }

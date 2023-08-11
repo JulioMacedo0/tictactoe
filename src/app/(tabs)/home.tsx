f2d2302937e5ce69710f03acc9dfca8ab2daadbf
Julio Macedo <juliocesar1076@gmail.com>
2023-08-11 16:55:45 -0300

    feat: remove unused code and add user data

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -2,28 +2,16 @@ import { View } from "react-native";
 import { Profile } from "@components/profile";
 import { Card } from "@/components/card";
 import { useRouter } from "expo-router";
-import { supabase } from "@/supabase/init";
-import { useEffect, useState } from "react";
-import { Session } from "@supabase/supabase-js";
+import { useAuth } from "@/context/authContext";
 
 export default function HomePage() {
   const navigate = useRouter();
-  const [user, setUser] = useState<Session>(null);
-
-  const getAuthSession = async () => {
-    const { data } = await supabase.auth.getSession();
-    setUser(data.session);
-  };
-  useEffect(() => {
-    getAuthSession();
-  }, []);
-
-  console.log(user);
+  const { user } = useAuth();
   return (
     <View className="flex-1 items-center bg-background ">
       <Profile
         imageUrl="https://avatars.githubusercontent.com/u/57598810?v=4"
-        userName="Julio Macedo"
+        userName={`${user.user_metadata.username}`}
       />
       <Card
         variant="rose"

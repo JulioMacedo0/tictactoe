a9f44103947c2586e34bb89fa5613feda081f5f8
Julio Macedo <juliocesar1076@gmail.com>
2023-08-16 16:06:00 -0300

    feat: connect channel

--- src/app/playground.tsx
+++ src/app/playground.tsx
@@ -1,12 +1,64 @@
+import { Card } from "@/components/card";
 import { Profile } from "@/components/profile";
 import { RoundCounter } from "@/components/round-counter";
 import { TicTacToe } from "@/components/tic-tac-toe";
 import { useAuth } from "@/context/authContext";
+import { supabase } from "@/supabase/init";
+import { useEffect, useState } from "react";
 
 import { View } from "react-native";
 
 function PlayGround() {
   const { user } = useAuth();
+  console.log("PlayGround render");
+  const [channel, setChannel] = useState(
+    supabase.channel("lobby", {
+      config: {
+        broadcast: {
+          ack: true,
+        },
+      },
+    })
+  );
+
+  const cleanSubscribe = async () => {
+    console.log(`cleanSubscribe channel state: ${channel.state}`);
+    const resp = await channel.unsubscribe();
+    console.log(`${user.user_metadata.username} exit subscribe is: ${resp}`);
+  };
+
+  const sendMsg = async () => {
+    console.log(`sendMsg channel state: ${channel.state}`);
+    const resp = await channel.send({
+      type: "broadcast",
+      event: "tic-tac",
+      payload: {
+        message: "hello, world",
+      },
+    });
+
+    console.log(`send message from ${user.user_metadata.username} : ${resp}`);
+  };
+
+  useEffect(() => {
+    console.log("useEffect runs");
+
+    const channelResp = channel
+      .on("broadcast", { event: "tic-tac" }, ({ payload }) =>
+        alert(payload.message)
+      )
+      .subscribe((status) =>
+        console.log(
+          `${user.user_metadata.username} subscribe channel => ${status}`
+        )
+      );
+    setChannel(channelResp);
+
+    return () => {
+      cleanSubscribe();
+    };
+  }, []);
+
   return (
     <View className="flex-1 bg-background">
       <View className="flex-row px-6 justify-between  mt-4">
@@ -24,7 +76,7 @@ function PlayGround() {
           playWith="O"
         />
       </View>
-
+      <Card text="Send msg" variant="gold" onPress={() => sendMsg()} />
       <TicTacToe />
     </View>
   );

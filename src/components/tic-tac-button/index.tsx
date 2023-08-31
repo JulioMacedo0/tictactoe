8c7d8f1d0c5fafea91e37117d808c6732e7202ce
Julio Macedo <juliocesar1076@gmail.com>
2023-08-31 11:13:07 -0300

    feat: add logic

--- src/components/tic-tac-button/index.tsx
+++ src/components/tic-tac-button/index.tsx
@@ -1,12 +1,16 @@
 import { useColors } from "@/hooks/use-colors";
 import { Pressable } from "react-native";
+import { X } from "../x";
+import { O } from "../o";
 
 interface TicTacToeButtonProps {
   onPress?: () => void;
+  icon: "X" | "O" | null;
 }
 
-export const TicTacToeButton = ({ onPress }: TicTacToeButtonProps) => {
+export const TicTacToeButton = ({ onPress, icon }: TicTacToeButtonProps) => {
   const { secundary } = useColors();
+
   return (
     <Pressable
       style={({ pressed }) => [
@@ -17,6 +21,8 @@ export const TicTacToeButton = ({ onPress }: TicTacToeButtonProps) => {
         },
       ]}
       onPress={onPress}
-    />
+    >
+      {icon == "X" ? <X size={45} /> : icon == "O" ? <O size={45} /> : null}
+    </Pressable>
   );
 };

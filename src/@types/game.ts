86b22ca6d77ea99894c3ccb7af75f076f8289744
Julio Macedo <juliocesar1076@gmail.com>
2023-08-31 11:12:35 -0300

    feat: add type

---
+++ src/@types/game.ts
@@ -0,0 +1,12 @@
+export type Boad = {
+  1: BoardValue;
+  2: BoardValue;
+  3: BoardValue;
+  4: BoardValue;
+  5: BoardValue;
+  6: BoardValue;
+  7: BoardValue;
+  8: BoardValue;
+};
+
+export type BoardValue = null | "X" | "O";

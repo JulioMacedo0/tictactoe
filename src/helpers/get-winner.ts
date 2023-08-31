f88dc8a682ab07aca15318138fe10bec7662e60c
Julio Macedo <juliocesar1076@gmail.com>
2023-08-31 17:46:27 -0300

    feat: create getWinner

---
+++ src/helpers/get-winner.ts
@@ -0,0 +1,23 @@
+import { Board } from "@/@types/game";
+
+export const getWinner = (board: Board) => {
+  const WinnerCondition = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+
+  for (const combination of WinnerCondition) {
+    const [a, b, c] = combination;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+
+  return null;
+};

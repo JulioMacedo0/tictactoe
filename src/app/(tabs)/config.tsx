8d27ae6ab24bc23d05a98ac135b7cbf54465131d
Julio Macedo <juliocesar1076@gmail.com>
2023-08-31 11:12:58 -0300

    feat: add logout button

--- src/app/(tabs)/config.tsx
+++ src/app/(tabs)/config.tsx
@@ -1,10 +1,14 @@
+import { Card } from "@/components/card";
+import { useAuth } from "@/context/authContext";
 import { Link, Stack, Tabs } from "expo-router";
 import { StyleSheet, View, Text } from "react-native";
 
 export default function ConfigPage() {
+  const { logout } = useAuth();
   return (
     <View className="">
       <Text>Config</Text>
+      <Card text="Logout" variant="rose" onPress={() => logout()} />
     </View>
   );
 }

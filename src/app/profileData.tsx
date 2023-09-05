12a19be64512eba7464136acd401b5d32812aee6
Julio Macedo <juliocesar1076@gmail.com>
2023-09-05 18:33:34 -0300

    feat: create profile page

---
+++ src/app/profileData.tsx
@@ -0,0 +1,12 @@
+import { Link, Stack, Tabs } from "expo-router";
+import { StyleSheet, View, Text } from "react-native";
+import LottieView from "lottie-react-native";
+export default function ProfileDataPage() {
+  return (
+    <View className="bg-background  flex-1">
+      <View className="h-80 w-80 mx-auto  ">
+        <LottieView source={require("@lotties/comming_soon.json")} autoPlay />
+      </View>
+    </View>
+  );
+}

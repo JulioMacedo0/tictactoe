5b4b6d6a5048f4072ccabf03e5b04981c64fd138
Julio Macedo <juliocesar1076@gmail.com>
2023-09-05 11:48:36 -0300

    feat: add animtaed lottie

--- src/app/(tabs)/history.tsx
+++ src/app/(tabs)/history.tsx
@@ -1,10 +1,12 @@
 import { Link, Stack, Tabs } from "expo-router";
 import { StyleSheet, View, Text } from "react-native";
-
+import LottieView from "lottie-react-native";
 export default function HistoryPage() {
   return (
-    <View className="">
-      <Text>History</Text>
+    <View className="bg-background  flex-1">
+      <View className="h-80 w-80 mx-auto  ">
+        <LottieView source={require("@lotties/comming_soon.json")} autoPlay />
+      </View>
     </View>
   );
 }
